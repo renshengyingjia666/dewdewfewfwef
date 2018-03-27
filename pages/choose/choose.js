9107a37d7cd0516700e7cf087eb2b11545311fd8
chen <401823611@qq.com>
2018-03-27 17:51:49 +0800

    第一次提交

---
+++ pages/choose/choose.js
@@ -0,0 +1,55 @@
+var app = getApp()
+Page({
+  data: {
+    /** 
+        * 页面配置 
+        */
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换  
+    currentTab: 0,
+    products:[1,2,3,4,5],
+  },
+  onLoad: function () {
+    var that = this;
+
+    /** 
+     * 获取系统信息 
+     */
+    wx.getSystemInfo({
+
+      success: function (res) {
+       
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: Math.ceil(that.data.products.length / 2) * 190,
+        });
+      }
+
+    });
+  },
+  /** 
+     * 滑动切换tab 
+     */
+  bindChange: function (e) {
+
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+
+  },
+  /** 
+   * 点击tab切换 
+   */
+  swichNav: function (e) {
+
+    var that = this;
+
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  }
+})  
